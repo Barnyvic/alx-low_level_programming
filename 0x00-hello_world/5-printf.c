520b16f9d473af88597456aafa4a7470f20c2dba
Barnyvic <victorbarny4@gmail.com>
2022-03-10 04:02:06 -0800

    six

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/*
- * main - Entry point
- *
- * Return: Always 0 (Success)
+/**
+ * main - prints the phrase with proper grammer,
+ *but the outcome is a piece of art,
+ * Return: 0 if excited properly, non-zero otherwise
  */
 int main(void)
 {
-	printf("with proper grammer , but the outcome is a piece of art,\n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
