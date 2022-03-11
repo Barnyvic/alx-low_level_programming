4eca29869f315d14803885d7afc7044e718258f2
Barnyvic <victorbarny4@gmail.com>
2022-03-11 03:39:02 -0800

    Task 4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - represent Entry Point to the program
+ * Return: 0 to stop the program
+ */
+int main(void)
+
+{
+
+	char a;
+	for (a = 'a'; a <= 'z' ; a++)
+		putchar(a);
+	for (a = 'A'; a <= 'Z'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+
+}
