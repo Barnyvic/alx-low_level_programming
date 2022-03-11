badb6296d914f0b81e2c8d10f7a1b41e41612fea
Barnyvic <victorbarny4@gmail.com>
2022-03-11 09:41:07 -0800

    11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,21 +13,21 @@ int main(void)
 			for (m = 49; m < 58; m++)
 			{
 				for (l = 50; l < 58; l++)
-
-				   {																	if (l > m && m > n)
-				      	{
+				{
+					if (l > m && m > n)
+					{
 						putchar(n);
 						putchar(m);
 						putchar(l);
-						if (n != 55 || m != 56)
+						if (n !=55 || m != 56)
 						{
 							putchar(',');
 							putchar(' ');
 						}
 					}
-				    }
+				}
 			}
 		}
-	putchar('\n');
-	return (0);
+		putchar('\n');
+		return (0);
 }
